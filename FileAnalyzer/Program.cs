e28a56c0cf5f9adf857a14c5c482b13069b5ae07
BeyzaElifYener <f211220009@ktun.edu.tr>
2025-08-13 15:39:52 +0300

    update Program.cs and project file

--- FileAnalyzer/Program.cs
+++ FileAnalyzer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileAnalyzer.Logging;
 
 namespace FileAnalyzer
 {
@@ -10,7 +11,20 @@ namespace FileAnalyzer
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("selam");
+            Logger.EnsureLogsFolder();
+            Logger.Info("Program Başlatıldı.");
+            try
+            {
+                Console.WriteLine("Bu bir test logudur.");
+                Logger.Info("Test logu yazıldı.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Bir hata oluştu", ex);
+            }
+
+            Console.WriteLine("Bitti. Çıkmak için bir tuşa basın...");
+            Console.ReadKey();
         }
     }
 }
